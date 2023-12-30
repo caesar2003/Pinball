6b755f3631bf320b72ae9a99ac382cf0bd94280c
Julius <uhoeb@student.kit.edu>
2023-12-30 18:18:27 +0100

    bkg2 and master changes

--- main.py
+++ main.py
@@ -3,73 +3,35 @@ from pathlib import Path
 
 # Initialize PyGame
 pygame.init()
-
-# Initial window size
-s_width = 600
-s_height = 800
-
-# Define spacetime 
-GRAVITY_X = 0.0
-GRAVITY_Y = 0.3
-DT = 1 # ms (discretization of time) 
-
-# Making display screen
-screen = pygame.display.set_mode((s_width, s_height), pygame.RESIZABLE)
-bg_orig=pygame.image.load(Path(__file__).parents[0] / Path("bkg.jpg")).convert()
+screen = pygame.display.set_mode((500,700))
+pygame.display.set_caption('Pinball ')
 clock = pygame.time.Clock()
-
-# Setup 
+test_font = pygame.font.Font(None,50)
 running = True
 
-# You could declare components (the initial ball, the other items, ...) here
-
-ball_x = 100
-ball_y = 150
-ball_vx = 0
-ball_vy = 0
-
-ball_radius = 30
-
-ball2_x = 200
-ball2_y = 150
-ball2_vx = 0
-ball2_vy = 0
-
-ball2_radius = 40
-
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+pic_surface = pygame.image.load('bkg2.jpg').convert_alpha()
+text_surface = test_font.render('Test', True, 'Green' )
+x_pos = 0
 
 # Main event loop
-while running:
+while True:
+    
+####################################
     for event in pygame.event.get():
-        # Get's all the user action (keyboard, mouse, joysticks, ...)
-        continue
-
-    # Adjust screen
-    s_width, s_height = screen.get_width(), screen.get_height()
-    bg = pygame.transform.scale(bg_orig, (s_width, s_height))
-    screen.blit(bg, (0, 0)) # redraws background image
-
-
-    # Here the action could take place
-
-    # s = s0 + v0*t + 1/2a*t**2
-
-    ball_vy = ball_vy + GRAVITY_Y*DT
-    ball2_vy = ball2_vy + GRAVITY_Y*DT
-
-    if ball_y >= screen.get_height():
-        ball_vy = ball_vy * (-1)
-    if ball2_y >= screen.get_height():
-        ball2_vy = ball2_vy * (-1)
-
-    ball_y = ball_y + ball_vy*DT + 0.5 * GRAVITY_Y*DT**2
-    ball2_y = ball2_y + ball2_vy*DT + 0.5 * GRAVITY_Y*DT**2
-
-    pygame.draw.circle(screen, (35, 161, 224), [ball_x,ball_y] , ball_radius)
-    pygame.draw.circle(screen, (35, 161, 224), [ball2_x,ball2_y] , ball2_radius)
-
-
-    pygame.display.flip() # Update the display of the full screen
-    clock.tick(60) # 60 frames per second
-
-# Done! Time to quit.
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+########################################
+    if running:
+        screen.blit(pic_surface, (0,0))
+        x_pos +=1
+        screen.blit(test_surface, (x_pos,100))
+        screen.blit(text_surface, (100,100))
+        pygame.draw.circle(screen, 'blue', (10,10), 10)
+    else:
+       screen.blit(pic_surface, (0,0))     
+    pygame.display.update()
+    clock.tick(60)
+   
