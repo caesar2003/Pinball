2e644546ca170714876018bb18770486d05c488c
Julius <uhoeb@student.kit.edu>
2024-01-25 16:15:13 +0100

    delete old files

--- Lukas_Pinball/main.py
+++ Lukas_Pinball/main.py
@@ -21,7 +21,7 @@ pygame.init()
 
 # preparation
 Ball(width / 2, height / 2)
-a = Wall([200, 500], [500, 300], [70, 200])
+a = Wall([10, 10], [500, 300], [70, 200])
 # print(str(a.rotations[0]))
 # print(str(a.rotations[1]))
 # print(str(a.rotations[2]))
