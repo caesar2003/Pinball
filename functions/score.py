aecb97b9d539e9a889102ebf74b1a624e1a6a42a
Julius <uhoeb@student.kit.edu>
2024-02-07 15:32:49 +0100

    score fixes

--- functions/score.py
+++ functions/score.py
@@ -18,19 +18,19 @@ from vars.setup import clock
 from vars.setup import screen, bg, bg_rect, width, height
 from vars.setup import wall_group, ball_group, circle_group, flipper_group, shot_group
 
-score_written = False
+#score_written = False
 def score(current_score, lives):
-    global score_written
+    #global score_written
     font = pygame.font.SysFont(None, 36)
     text = font.render(f'Score:{round(current_score)}', True, (237, 178, 106))
     # Textposition
     text_rect = text.get_rect()
     text_rect.center = (width / 2-30, height-20)  
     screen.blit(text, text_rect)
-    if lives == 0 and not score_written:
+    if lives == 0:
         with open("sonstiges/data.txt", "a") as file:
                 file.write(f'{round(current_score)}\n')
-                score_written = True
+                #score_written = True
     elif lives!= 0:
         current_score += 1/60
     for ball in ball_group:
@@ -50,17 +50,17 @@ def new_ball_spawn(ball_group, lives):
         sound.new_ball_sound()
         lives = 3
     for ball in ball_group:
-        if  ball.coords.y > ball_coord_death and lives == 3:
-            ball.kill()
+        if  (ball.coords.y > height or ball.coords.y < 0 or ball.coords.x < 0 or ball.coords.x > width) and lives == 3:
             new_ball()
+            ball.kill()
             sound.new_ball_sound()
             lives = 2
-        elif ball.coords.y > ball_coord_death and lives == 2:
-            ball.kill()
+        elif (ball.coords.y > height or ball.coords.y < 0 or ball.coords.x < 0 or ball.coords.x > width) and lives == 2:
             new_ball()
+            ball.kill()
             sound.new_ball_sound()
             lives = 1
-        elif ball.coords.y > ball_coord_death and lives ==1:
+        elif (ball.coords.y > height or ball.coords.y < 0 or ball.coords.x < 0 or ball.coords.x > width) and lives ==1:
             ball.kill()
             lives = 0
 
